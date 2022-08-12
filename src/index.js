8ff95a422f3b0d5b0830dc8e162a4ac1bcb3eda6
Mulumia Baraka Mshindi <65751779+Baraka-Mulumia@users.noreply.github.com>
2022-08-12 09:15:49 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,20 +1,19 @@
 import { APP_CONFIG } from "./config/appConfig";
 import Express from "express";
-import Logger from "./utils/logger";
 import cors from "cors";
+import { infoLog } from "./utils/logger";
 import morgan from "morgan";
 
 const app = Express();
 app.use(cors());
 app.use(Express.json());
+app.use(morgan("dev"));
 
 app.get("/", (req, res) => {
-  res.send("Hello World");
-  Logger.log("Hello World", "info", "index.mjs");
+    res.send("Hello World");
+    infoLog("You have hit the root endpoint");
 });
 
 app.listen(APP_CONFIG.port, () => {
-  console.log(
-    `${APP_CONFIG.appName} Server is running on port ${APP_CONFIG.port}`
-  );
+    infoLog(`${APP_CONFIG.appName} Server is running on port ${APP_CONFIG.port}`);
 });
