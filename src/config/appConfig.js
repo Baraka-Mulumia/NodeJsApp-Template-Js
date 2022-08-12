3c0a8ed8496eab87c46f4155fac4882424f36abe
Mulumia Baraka Mshindi <65751779+Baraka-Mulumia@users.noreply.github.com>
2022-08-12 09:15:35 +0300

    Update appConfig.js

--- src/config/appConfig.js
+++ src/config/appConfig.js
@@ -4,16 +4,16 @@ dotenv.config();
 
 // config.js
 export const APP_CONFIG = {
-  port: process.env.DEV_APP_PORT || 3000,
-  appName: process.env.APP_NAME || "node-app",
-  env: process.env.NODE_ENV || "production",
+    port: process.env.DEV_APP_PORT || 1337,
+    appName: process.env.APP_NAME || "node-app",
+    env: process.env.NODE_ENV || "production",
 };
 
 export const LOG_CONFIG = {
-  level: process.env.LOG_LEVEL || "debug",
-  logPath: "/node-app/logs/",
+    level: process.env.LOG_LEVEL || "debug",
+    logPath: "/node-app/logs/",
 };
 
 export const DB_CONFIG = {
-  mongoDBUrl: process.env.MONGODB_URI || "mongodb://localhost:27017/node-app",
+    mongoDBUrl: process.env.MONGODB_URI || "mongodb://localhost:27017/node-app",
 };
